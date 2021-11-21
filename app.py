b11ae3e65f890ee2d36b7d5cdc159e5076c2403c
moshe <barboym@yahoo.com>
2021-11-21 20:54:55 +0200

    finally a fully working version

--- app.py
+++ app.py
@@ -7,12 +7,13 @@ import base64
 from io import BytesIO
 import tensorflow as tf
 import numpy as np
-
+import cv2
 
 model = tf.keras.models.load_model("static/best_model.hdf5")
 
 def preprocess_image(image_txt):
-    decoded_img = base64.b64decode()
+    image_txt2 = image_txt.split(",")[-1]
+    decoded_img = base64.b64decode(bytes(image_txt2,'utf-8'))
     img = Image.open(BytesIO(decoded_img))
     img = np.array(img).max(axis=2)
     img = cv2.resize(img, (28,28), interpolation= cv2.INTER_LINEAR)
@@ -23,14 +24,12 @@ def preprocess_image(image_txt):
 def home():
     # print("\n",dict(request.form)["image"],"\n")
     # print(image_txt)
-    image_txt = dict(request.form).get("image","no_image")
-    if image_txt=="no_image":
-        return render_template("index.html",res="")
-    else:
-        preprocessed_image = preprocess_image(image_txt)
-        input_image = preprocessed_image[np.newaxis,...,np.newaxis]
-        result = model.predict(input_image).argmax(axis=1)[0]
-        return render_template("index.html",res=result)
+    image_txt = dict(request.form)["image"]
+    # print(image_txt)
+    preprocessed_image = preprocess_image(image_txt)
+    input_image = preprocessed_image[np.newaxis,...,np.newaxis]
+    result = model.predict(input_image).argmax(axis=1)[0]
+    return render_template("index.html",res=result)
 
 
 if __name__ == '__main__':
