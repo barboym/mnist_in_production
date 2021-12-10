c2573b090f06190f403f005e3f3ad1b59d7ec940
moshe <barboym@yahoo.com>
2021-12-10 13:19:46 +0200

    good row alignment

--- app.py
+++ app.py
@@ -2,40 +2,52 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
-from PIL import Image
 import base64
-from io import BytesIO
 import tensorflow as tf
 import numpy as np
-import cv2
 
 app = Flask(__name__)
 model = tf.keras.models.load_model("static/best_model.hdf5")
 
-def preprocess_image(image_txt):
-    image_txt2 = image_txt.split(",")[-1]
-    decoded_img = base64.b64decode(bytes(image_txt2,'utf-8'))
-    img = Image.open(BytesIO(decoded_img))
-    img = np.array(img).max(axis=2)
-    img = cv2.resize(img, (28,28), interpolation= cv2.INTER_LINEAR)
-    img = img / (img.max() + 10**-8)
-    return img
 
-@app.route("/",methods=["GET","POST"])
+def covert_image_from_txt_to_tensor(txt_image) -> tf.constant:
+    """
+    converting the image from base64 to bytes and then to tensor
+    :param txt_image:
+    :return: tensor
+    """
+    base64_img = base64.b64decode(bytes(txt_image, 'utf-8'))
+    tensor_img = tf.image.decode_image(base64_img)
+    return tensor_img
+
+
+def resize_to_model_input_shape_and_normalize(tensor_img: tf.constant):
+    tensor_img = tf.image.resize(tensor_img, model.input.shape[1:3])
+    tensor_img = tf.reduce_max(tensor_img, axis=2, keepdims=True)
+    tensor_img = tensor_img[np.newaxis, ...]
+    tensor_img = tensor_img / (tf.reduce_max(tensor_img) + 10 ** -8)
+    return tensor_img
+
+
+def preprocess_image(image_txt) -> tf.constant:
+    tensor_img = covert_image_from_txt_to_tensor(image_txt.split(",")[-1])
+    tensor_img = resize_to_model_input_shape_and_normalize(tensor_img)
+    return tensor_img
+
+
+@app.route("/", methods=["GET", "POST"])
 def home():
-    # print("\n",dict(request.form),"\n")
-    # print(image_txt)
     image_txt = dict(request.form).get("image", 0)
-    # print(image_txt)
-    if image_txt==0:
-        return render_template("index.html",res="")
-    preprocessed_image = preprocess_image(image_txt)
-    input_image = preprocessed_image[np.newaxis,...,np.newaxis]
+    if image_txt == 0:
+        return render_template("index.html", res="")
+
+    input_image = preprocess_image(image_txt)
+    input_image_to_send = tf.image.resize(input_image, (200, 200))[0].numpy().tobytes()
     result = model.predict(input_image).argmax(axis=1)[0]
-    return render_template("index.html",res=result)
+    return render_template("index.html", result=result, input_image=input_image_to_send)
 
 
 if __name__ == '__main__':
-    # app.debug = True
-    app.run()
-    # app.run(debug = True)
+    app.debug = True
+    # app.run()
+    app.run(debug = True)
